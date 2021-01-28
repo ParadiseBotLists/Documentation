a7805a129d93bebfd2ecd6db800d7c50db770d3e
Toxic Dev <59587139+TheRealToxicDev@users.noreply.github.com>
2021-01-28 16:22:03 -0700

    Update index.js

--- src/components/svg/advertise/index.js
+++ src/components/svg/advertise/index.js
@@ -8,14 +8,14 @@ export default () => {
   const [ref, inView] = useInView({ triggerOnce: true, threshold: 0.75 });
 
   return (
-    <svg style="fill: #758acf;" ref={ref}
+    <svg style={{ fill: '#758acf' }} ref={ref}
       className={classnames(styles.svg, { [styles.active]: inView })}
       viewBox="0 0 156 337"
       fill="none"
       xmlns="http://www.w3.org/2000/svg">
       <image 
          href="https://www.freepnglogos.com/uploads/advertising-png/advertising-icon-computer-mouse-ads-screen-15.jpg"
-         style={{ height: 200, width: 300, }}
+         style={{ height: 200, width: 400, fill: '#758acf'}}
       />
     </svg>
   );
