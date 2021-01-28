b8dc884d2f745836fb3b645b155b6914828c42e7
Toxic Dev <59587139+TheRealToxicDev@users.noreply.github.com>
2021-01-28 13:17:54 -0700

    Update index.js

--- src/components/svg/cert/index.js
+++ src/components/svg/cert/index.js
@@ -8,8 +8,12 @@ export default () => {
   const [ref, inView] = useInView({ triggerOnce: true, threshold: 0.75 });
 
   return (
-    <svg className= xmlns="http://www.w3.org/2000/svg">
-      <image href="https://mdn.mozillademos.org/files/6457/mdn_logo_only_color.png" height="200" width="200"/>
+    <svg ref={ref}
+      className={classnames(styles.svg, { [styles.active]: inView })}
+      viewBox="0 0 156 337"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg">
+      <image href="https://media.discordapp.net/attachments/747390406722977863/760792636436250644/cert.png" height="100" width="100"/>
     </svg>
   );
 };
