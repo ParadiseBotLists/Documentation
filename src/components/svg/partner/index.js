2ab9699c9683e0675b4e75022abb0cc72ef0b3bd
Toxic Dev <59587139+TheRealToxicDev@users.noreply.github.com>
2021-01-28 13:24:51 -0700

    Create index.js

---
+++ src/components/svg/partner/index.js
@@ -0,0 +1,19 @@
+import classnames from "classnames";
+import React from "react";
+import { useInView } from "react-intersection-observer";
+
+import styles from "./styles.module.css";
+
+export default () => {
+  const [ref, inView] = useInView({ triggerOnce: true, threshold: 0.75 });
+
+  return (
+    <svg ref={ref}
+      className={classnames(styles.svg, { [styles.active]: inView })}
+      viewBox="0 0 156 337"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg">
+      <image href="https://media.discordapp.net/attachments/747390406722977863/760792636436250644/cert.png" height="100" width="100"/>
+    </svg>
+  );
+};
