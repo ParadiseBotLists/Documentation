13c1ae13e9848d6e9e6993e5151e4cbbc8413873
Toxic Dev <59587139+TheRealToxicDev@users.noreply.github.com>
2021-01-28 15:08:15 -0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -26,7 +26,7 @@ const features = [
         bigger deals.
       </>
     ),
-    Component: QueueSvg,
+    /* Component: QueueSvg, */
   },
   {
     title: <>Hello Advertising Freedom</>,
@@ -37,7 +37,7 @@ const features = [
         your developers to focus on higher-priority work.
       </>
     ),
-    Component: TeamDocsSvg,
+    /* Component: TeamDocsSvg, */
   },
 ];
 
@@ -71,7 +71,7 @@ function Home() {
       <main>
         <section className={styles.features}>
           <div className="container">
-            {features.map(({ title, description, Component }, idx) => (
+            {features.map(({ title, description, /* Component } */, idx) => (
               <div key={idx} className={classnames("row", styles.row)}>
                 <div className="col">
                   <TitleWithMarker title={title} />
