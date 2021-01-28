325cd7e80b966c22f5967240d222fc0fb52bc5a1
Toxic Dev <59587139+TheRealToxicDev@users.noreply.github.com>
2021-01-28 13:27:12 -0700

    Update index.js

--- src/components/svg/partner/index.js
+++ src/components/svg/partner/index.js
@@ -13,7 +13,7 @@ export default () => {
       viewBox="0 0 156 337"
       fill="none"
       xmlns="http://www.w3.org/2000/svg">
-      <image href="https://media.discordapp.net/attachments/747390406722977863/760792636436250644/cert.png" height="100" width="100"/>
+      <image href="https://cdn.discordapp.com/attachments/747390406722977863/760791912642117661/image0.png" height="100" width="100"/>
     </svg>
   );
 };
