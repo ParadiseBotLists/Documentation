bf1c3119dded905bdfb17a04bfc272ed82822462
Toxic Dev <59587139+TheRealToxicDev@users.noreply.github.com>
2021-01-28 16:48:59 -0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -63,7 +63,7 @@ function Home() {
               className={styles.heroImage}
               src={"/img/ParadiseBotsLogo.png"}
               alt="Paradise Bots Logo"
-              style={{ width: 300, height: 300, position: top -30 }}
+              style={{ width: 300, height: 300, position: top -30px }}
             />
           )}
         </header>
