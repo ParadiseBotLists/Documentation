2900a9738341228e7fca774010ec449b96c7c277
Toxic Dev <59587139+TheRealToxicDev@users.noreply.github.com>
2021-01-28 16:14:47 -0700

    Update index.js

--- src/components/svg/advertise/index.js
+++ src/components/svg/advertise/index.js
@@ -15,7 +15,7 @@ export default () => {
       xmlns="http://www.w3.org/2000/svg">
       <image 
          href="https://www.freepnglogos.com/uploads/advertising-png/advertising-icon-computer-mouse-ads-screen-15.jpg"
-         style={{ height: 200, width: 200 }}
+         style={{ height: 200, width: 300 }}
       />
     </svg>
   );
