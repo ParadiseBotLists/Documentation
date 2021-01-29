228dd16f873952d4c612784b20d73b6ad7b0f5fc
Toxic Dev <59587139+TheRealToxicDev@users.noreply.github.com>
2021-01-28 19:44:15 -0700

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,8 +14,8 @@ module.exports = {
   ],
   themeConfig: {
     algolia: {
-      apiKey: "83fedff8cf4b3c8c307860d884c83b58",
-      indexName: "paradiseapp",
+      apiKey: "e446f4b89c6d2f86db157cc2906a1520",
+      indexName: "ossoapp",
       searchParamaters: {
         appId: "BH4D9OD16A",
       },
