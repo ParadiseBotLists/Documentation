44a9b9166714b5013212ad0c4d0c8770d0ed8e04
Toxic Dev <59587139+TheRealToxicDev@users.noreply.github.com>
2021-01-28 17:07:54 -0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -81,6 +81,7 @@ function Home() {
                 </div>
               </div>
             ))}
+              <iframe src="https://discordapp.com/widget?id=748977820457238530&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
           </div>
         </section>
       </main>
