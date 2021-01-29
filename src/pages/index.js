bac52542807acd99c7ba8db10beef7a64a5bdeb3
Toxic Dev <59587139+TheRealToxicDev@users.noreply.github.com>
2021-01-28 17:06:01 -0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,10 +20,9 @@ const features = [
     title: <>Goodbye Queue Times</>,
     description: (
       <>
-        Want to sell to Enterprise customers? SAML-based SSO is a requirement.
-        Osso integrates with all of the most popular Identity Providers so that
-        you can check off another box on your sales pitch and start closing
-        bigger deals.
+       Tired of waiting weeks on end for your bot to be approved? We are to that's why at Paradise Bots
+       your bot will be approved almost as fast as it was added, We strive for a maximum 3 day turnaround on queue times.
+       Regardless of the situation at hand there is usually always someone online and ready to test your bot. 
       </>
     ),
    
@@ -32,9 +31,12 @@ const features = [
     title: <>Hello Advertising Freedom</>,
     description: (
       <>
-        With its intuitive UI and custom docs, Osso will enable your customer
-        success teams (or users!) to handle onboarding and support, freeing up
-        your developers to focus on higher-priority work.
+       Tired of rules and regulations holding you back from your true potential?
+       Well worry no more, we don't care about the Discord Terms of Service anymore
+       then a normal discord user is expected to, With that being said we don't take to
+       kindly to the blatantly obvious (racism, lolis, nsfw content) aside from that we have
+       No limits as to what your bot can do or what features it has (auto role, auto change colors)
+       we don't care as long as you follow the obvious terms.
       </>
     ),
    
