54afd789579340156da5af198f3b7aebff5edd39
Toxic Dev <59587139+TheRealToxicDev@users.noreply.github.com>
2021-01-29 14:19:05 -0700

    Update index.js

--- netlify-forms/index.js
+++ netlify-forms/index.js
@@ -6,7 +6,6 @@ module.exports = function (context, options) {
         headTags: [],
         preBodyTags: [
           `<form name="email-list" netlify netlify-honeypot="bot-field" hidden><input type="email" name="email" /></form>`,
-          `<form name="plan-interest" netlify netlify-honeypot="bot-field" hidden><input type="email" name="email" /><input name="about" /><input name="chosenPlan" /></form>`,
         ],
         postBodyTags: [],
       };
