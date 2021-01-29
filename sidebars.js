047049e2ed43d1b5c683155e18a903675321bdc3
Toxic Dev <59587139+TheRealToxicDev@users.noreply.github.com>
2021-01-28 19:18:52 -0700

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -10,45 +10,5 @@ module.exports = {
         "paradise-api/paradiseapi.js",
       ],
     },
-    {
-      type: "category",
-      label: "How to deploy Osso",
-      items: [
-        "deploy/overview",
-        "deploy/env-variables",
-        "deploy/build-steps",
-        "deploy/post-deployment",
-      ],
-    },
-    {
-      type: "category",
-      label: "How to configure Osso",
-      items: [
-        "configure/overview",
-        "configure/adding-oauth-clients",
-        "configure/redirect-uris",
-        "configure/test-qa",
-      ],
-    },
-    {
-      type: "category",
-      label: "How to consume Osso",
-      items: [
-        "consume/overview",
-        "consume/consuming-osso",
-        "consume/passport-osso",
-        "consume/omniauth-osso",
-      ],
-    },
-    {
-      type: "category",
-      label: "How to use Osso",
-      items: [
-        "user-guide/overview",
-        "user-guide/adding-customers",
-        "user-guide/onboarding-customers",
-        "user-guide/self-serve",
-      ],
-    },
   ],
 };
